648f2dbee1e171ac377e775339e2259b92de56e0
59de44955ebd <59de44955ebd@gmail.com>
2024-07-22 16:27:47 +0200

    added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ruwps',
+    version='0.1.0',
+    description='Ridiculously Uncomplicated Windows Python Systray apps',
+    author='59de44955ebd',
+    url='https://github.com/59de44955ebd/ruwps',
+    packages=['ruwps'],
+)
